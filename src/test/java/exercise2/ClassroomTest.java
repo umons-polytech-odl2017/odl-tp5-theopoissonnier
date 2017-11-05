902469cba5b78697beb6dd6945a0ae4633e64e70
François Roland <francois.roland@umons.ac.be>
2017-11-05 20:33:19 +0100

    Write exercise 2 tests.

--- src/test/java/exercise2/ClassroomTest.java
+++ src/test/java/exercise2/ClassroomTest.java
@@ -5,132 +5,84 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.data.Offset.offset;
 
 public class ClassroomTest {
     private Classroom classroom;
-    private Set<String> availableCourses;
+    private Student thorosOfMyr;
+    private Student yaraGreyjoy;
+    private Student maesterAemon;
+    private Student myrcellaBaratheon;
+    private Student gregorClegane;
+    private Student branStark;
+    private Student osha;
+    private Student hodor;
 
     @Before
     public void fillInClassroom() {
-        availableCourses.add("sewing");
-        availableCourses.add("fencing");
-        availableCourses.add("archery");
-        availableCourses.add("gardening");
-        availableCourses.add("fighting");
-        availableCourses.add("politics");
-        availableCourses.add("running");
-        availableCourses.add("");
-
         classroom = new Classroom();
         Set<Student> students = new HashSet<>();
-        Student grandMaesterPycelle = new Student("Grand Maester Pycelle", "001001");
-        addStudent(grandMaesterPycelle, students, classroom);
-        Student merynTrant = new Student("Meryn Trant", "001002");
-        addStudent(merynTrant, students, classroom);
-        Student hodor = new Student("Hodor", "001003");
-        addStudent(hodor, students, classroom);
-        Student grenn = new Student("Grenn", "001004");
-        addStudent(grenn, students, classroom);
-        Student osha = new Student("Osha", "001005");
-        addStudent(osha, students, classroom);
-        Student rickonStark = new Student("Rickon Stark", "001006");
-        addStudent(rickonStark, students, classroom);
-        Student ros = new Student("Ros", "001007");
-        addStudent(ros, students, classroom);
-        Student gregorClegane = new Student("Gregor Clegane", "001008");
-        addStudent(gregorClegane, students, classroom);
-        Student janosSlynt = new Student("Janos Slynt", "001009");
-        addStudent(janosSlynt, students, classroom);
-        Student lancelLannister = new Student("Lancel Lannister", "001010");
-        addStudent(lancelLannister, students, classroom);
-        Student myrcellaBaratheon = new Student("Myrcella Baratheon", "001011");
-        addStudent(myrcellaBaratheon, students, classroom);
-        Student rodrikCassel = new Student("Rodrik Cassel", "001012");
-        addStudent(rodrikCassel, students, classroom);
-        Student maesterLuwin = new Student("Maester Luwin", "001013");
-        addStudent(maesterLuwin, students, classroom);
-        Student irri = new Student("Irri", "001014");
-        addStudent(irri, students, classroom);
-        Student doreah = new Student("Doreah", "001015");
-        addStudent(doreah, students, classroom);
-        Student kevanLannister = new Student("Kevan Lannister", "001016");
-        addStudent(kevanLannister, students, classroom);
-        Student barristanSelmy = new Student("Barristan Selmy", "001017");
-        addStudent(barristanSelmy, students, classroom);
-        Student rast = new Student("Rast", "001018");
-        addStudent(rast, students, classroom);
-        Student maesterAemon = new Student("Maester Aemon", "001019");
-        addStudent(maesterAemon, students, classroom);
-        Student pypar = new Student("Pypar", "001020");
-        addStudent(pypar, students, classroom);
-        Student alliserThorne = new Student("Alliser Thorne", "001021");
-        addStudent(alliserThorne, students, classroom);
-        Student othellYarwyck = new Student("Othell Yarwyck", "001022");
-        addStudent(othellYarwyck, students, classroom);
-        Student lorasTyrell = new Student("Loras Tyrell", "001023");
-        addStudent(lorasTyrell, students, classroom);
-        Student hotPie = new Student("Hot Pie", "001024");
-        addStudent(hotPie, students, classroom);
-        Student bericDondarrion = new Student("Beric Dondarrion", "001025");
-        addStudent(bericDondarrion, students, classroom);
-        Student podrickPayne = new Student("Podrick Payne", "001026");
-        addStudent(podrickPayne, students, classroom);
-        Student eddisonTollett = new Student("Eddison Tollett", "001027");
-        addStudent(eddisonTollett, students, classroom);
-        Student yaraGreyjoy = new Student("Yara Greyjoy", "001028");
-        addStudent(yaraGreyjoy, students, classroom);
-        Student selyseFlorent = new Student("Selyse Florent", "001029");
-        addStudent(selyseFlorent, students, classroom);
-        Student littleSam = new Student("Little Sam", "001030");
-        addStudent(littleSam, students, classroom);
-        Student greyWorm = new Student("Grey Worm", "001031");
-        addStudent(greyWorm, students, classroom);
-        Student qyburn = new Student("Qyburn", "001032");
-        addStudent(qyburn, students, classroom);
-        Student olennaTyrell = new Student("Olenna Tyrell", "001033");
-        addStudent(olennaTyrell, students, classroom);
-        Student shireenBaratheon = new Student("Shireen Baratheon", "001034");
-        addStudent(shireenBaratheon, students, classroom);
-        Student meeraReed = new Student("Meera Reed", "001035");
-        addStudent(meeraReed, students, classroom);
-        Student jojenReed = new Student("Jojen Reed", "001036");
-        addStudent(jojenReed, students, classroom);
-        Student thorosOfMyr = new Student("Thoros of Myr", "001037");
-        addStudent(thorosOfMyr, students, classroom);
-        Student olly = new Student("Olly", "001038");
-        addStudent(olly, students, classroom);
-        Student maceTyrell = new Student("Mace Tyrell", "001039");
-        addStudent(maceTyrell, students, classroom);
-        Student theWaif = new Student("The Waif", "001040");
-        addStudent(theWaif, students, classroom);
-        Student bowenMarsh = new Student("Bowen Marsh", "001041");
-        addStudent(bowenMarsh, students, classroom);
-    }
-
-    private static void addStudent(Student grandMaesterPycelle, Set<Student> students, Classroom classroom) {
-        classroom.addStudent(grandMaesterPycelle);
-        students.add(grandMaesterPycelle);
+        hodor = new Student("Hodor", "001003");
+        hodor.setScore("hunting", 12);
+        hodor.setScore("figthing", 14);
+        hodor.setScore("running", 10);
+        students.add(hodor);
+        osha = new Student("Osha", "001005");
+        osha.setScore("fighting", 14);
+        osha.setScore("cooking", 4);
+        osha.setScore("swimming", 4);
+        students.add(osha);
+        branStark = new Student("Bran Stark", "001006");
+        branStark.setScore("running", 0);
+        branStark.setScore("archery", 14);
+        students.add(branStark);
+        gregorClegane = new Student("Gregor Clegane", "001008");
+        gregorClegane.setScore("fighting", 18);
+        gregorClegane.setScore("running", 10);
+        gregorClegane.setScore("hunting", 16);
+        students.add(gregorClegane);
+        myrcellaBaratheon = new Student("Myrcella Baratheon", "001011");
+        myrcellaBaratheon.setScore("cooking", 14);
+        myrcellaBaratheon.setScore("swimming", 16);
+        myrcellaBaratheon.setScore("archery", 8);
+        students.add(myrcellaBaratheon);
+        maesterAemon = new Student("Maester Aemon", "001019");
+        maesterAemon.setScore("running", 2);
+        maesterAemon.setScore("cooking", 16);
+        maesterAemon.setScore("hunting", 17);
+        students.add(maesterAemon);
+        yaraGreyjoy = new Student("Yara Greyjoy", "001028");
+        yaraGreyjoy.setScore("swimming", 18);
+        yaraGreyjoy.setScore("fighting", 19);
+        yaraGreyjoy.setScore("hunting", 16);
+        students.add(yaraGreyjoy);
+        thorosOfMyr = new Student("Thoros of Myr", "001037");
+        thorosOfMyr.setScore("fighting", 15);
+        thorosOfMyr.setScore("archery", 10);
+        thorosOfMyr.setScore("cooking", 19);
+        students.add(thorosOfMyr);
     }
 
     @Test
-    public double averageScore() {
-        classroom.averageScore();
+    public void averageScore() {
+        assertThat(classroom.averageScore()).isCloseTo(12.48, offset(0.1));
     }
 
     @Test
-    public int countStudents() {
-        return 0;
+    public void countStudents() {
+        assertThat(classroom.countStudents()).isEqualTo(8);
     }
 
     @Test
-    public List<Student> topScorers(String course, int n) {
-        return null;
+    public void topScorers(String course, int n) {
+        assertThat(classroom.topScorers("fighting", 3)).containsExactly(yaraGreyjoy, gregorClegane, thorosOfMyr);
     }
 
     @Test
-    public List<Student> successfulStudents() {
-        return null;
+    public void successfulStudents() {
+        assertThat(classroom.successfulStudents()).containsExactly(yaraGreyjoy, gregorClegane, thorosOfMyr, myrcellaBaratheon, hodor);
     }
 }
